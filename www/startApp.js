fbe92f31530f72e0d3cdd3fe372cd765909b2dd1
Martin Gonzalez <tinchogon34@gmail.com>
2016-04-28 16:25:48 -0300

    Prueba 2

--- www/startApp.js
+++ www/startApp.js
@@ -1,15 +1,15 @@
 /**
 	com.lampa.startapp
 	https://github.com/lampaa/com.lampa.startapp
-	
+
 	Phonegap plugin for check or launch other application in android device (iOS support).
 	bug tracker: https://github.com/lampaa/com.lampa.startapp/issues
 */
 
 var exec = require('cordova/exec');
 
-module.exports = {
-	/** 
+var startApp = {
+	/**
 	 * Set application params
 	 *
 	 * @param {Mixed} params				params, view documentation https://github.com/lampaa/com.lampa.startapp
@@ -17,28 +17,28 @@ module.exports = {
 	 * @param {Function} errorCallback		The callback that is called when an error occurred when the program starts.
 	 *
 	 */
-	 
+
 	set: function(params, extra) {
 		var output = [params];
-			
+
 		if(extra != undefined) {
 			output.push(extra);
 		}
 		else {
 			output.push(null);
 		}
-		
+
 		return {
 			start: function(completeCallback, errorCallback) {
 				completeCallback = completeCallback || function() {};
 				errorCallback = errorCallback || function() {};
-				
+
 				exec(completeCallback, errorCallback, "startApp", "start", output);
 			},
 			check: function(completeCallback, errorCallback) {
 				completeCallback = completeCallback || function() {};
 				errorCallback = errorCallback || function() {};
-				
+
 				exec(completeCallback, errorCallback, "startApp", "check", output);
 			}
 		}
@@ -56,3 +56,5 @@ module.exports = {
 		this.getExtra(extraValue, completeCallback, errorCallback);
 	}
 }
+
+module.exports = startApp;
